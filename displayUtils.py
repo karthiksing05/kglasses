defcc0ed4729c00f4a99201ce65c72c33aff74e4
karthik <karthiksing05@gmail.com>
2022-07-23 15:54:37 -0400

    PY GAUGETTE IS THE META

--- displayUtils.py
+++ displayUtils.py
@@ -1,55 +1,17 @@
-from turtle import clear
-import board
-import digitalio
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
+import gaugette.gpio
+import gaugette.ssd1306
+from gaugette.fonts import arial_16
 
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
+RESET_PIN = 15
+DC_PIN    = 16
 
-# Change these to the right size for your display!
-WIDTH = 128
-HEIGHT = 64
-BORDER = 5
+_gpio = gaugette.gpio.GPIO()
+_spi = gaugette.spi.SPI(bus=0, device=0)
+oled = gaugette.ssd1306.SSD1306(_gpio, _spi, reset_pin=RESET_PIN, dc_pin=DC_PIN)
+oled.begin()
 
-# Use for SPI
-spi = board.SPI()
-oled_cs = digitalio.DigitalInOut(board.D5)
-oled_dc = digitalio.DigitalInOut(board.D6)
-oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
+fonttypes = {}
 
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
+def write_text(text:str, coordx:int, coordy:int, fonttype:str):
+    pass
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-fonttypes = {
-    "header":("/home/pi/kglasses/fonts/tahomabold.ttf", 24),
-    "body":("/home/pi/kglasses/fonts/minecraftia.ttf", 16),
-    "special":("/home/pi/kglasses/fonts/magneto.ttf", 24)
-}
-
-def clear_display():
-    oled.fill(0)
-    oled.show()
-
-def update_image():
-    oled.image(image)
-    oled.show()
-
-def write_text(text:str, coords:tuple, fonttype:str):
-
-    font = ImageFont.truetype(fonttypes[fonttype][0], fonttypes[fonttype][1])
-
-    # Draw Some Text
-    draw.text(coords, text, font=font, fill=255)
-
-    update_image()
-
-# Helper Functions:
-def show_home_screen():
-    clear_display()
-
-
-clear_display()
