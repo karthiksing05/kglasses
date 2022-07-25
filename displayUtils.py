0a1bca779a32a06769615878cbe9f5e4e25347a9
karthik <karthiksing05@gmail.com>
2022-07-25 19:53:31 -0400

    OMG whatsapp messaging in the matrix!!! gonna optimize everything for linux next, add display control functions and get ready for iteration 1!

--- displayUtils.py
+++ displayUtils.py
@@ -1,17 +1,42 @@
 import gaugette.gpio
 import gaugette.ssd1306
-from gaugette.fonts import arial_16
+import gaugette
+from gaugette.fonts import tahoma_16, arial_24, stencil_33, magneto_16
+import datetime
+import time
 
 RESET_PIN = 15
 DC_PIN    = 16
 
 _gpio = gaugette.gpio.GPIO()
 _spi = gaugette.spi.SPI(bus=0, device=0)
-oled = gaugette.ssd1306.SSD1306(_gpio, _spi, reset_pin=RESET_PIN, dc_pin=DC_PIN)
+oled = gaugette.ssd1306.SSD1306(_gpio, _spi, reset_pin=RESET_PIN, dc_pin=DC_PIN, rows=64, cols=128)
 oled.begin()
 
-fonttypes = {}
+fonttypes = {
+    "body":tahoma_16,
+    "header":arial_24,
+    "time": stencil_33,
+    "date": magneto_16
+}
+
+def clear_display():
+    oled.clear_display()
 
 def write_text(text:str, coordx:int, coordy:int, fonttype:str):
-    pass
+    
+    font = fonttypes[fonttype]
+    
+    oled.draw_text3(coordx, coordy, text, font)
+    
+    oled.display()
 
+def display_home_screen():
+    clear_display()
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    write_text(time, 0, 0, "time")
+
+if __name__ == "__main__":
+    while True:
+        display_home_screen()
+        time.sleep(0.1)
---
+++ messaging.py
@@ -0,0 +1,75 @@
+import webbrowser as web
+import pyautogui as pg
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+
+import time
+import datetime
+import pickle
+import os
+import glob
+
+class Messager(object):
+
+    def __init__(self, load_contacts_by_def=True):
+        self.contactsFN = "data\\contacts.pickle"
+        self.width, self.height = pg.size()
+
+        if load_contacts_by_def:
+            self.load_contacts()
+
+    def load_contacts(self):
+        with open(self.contactsFN, "rb") as f:
+            self.contacts = pickle.load(f)
+
+    def add_new_contact(self, name:str, number:str):
+        if "+" not in number:
+            number = "+1" + number
+        self.contacts[name] = number
+    
+    def save_contacts(self):
+        with open(self.contactsFN, "wb") as f:
+            pickle.dump(self.contacts, f)
+
+    def send_message(self, number:str, text:str):
+        web.open(f"https://web.whatsapp.com/send?phone={number}&text={quote(text)}")
+        time.sleep(3)
+        pg.click(self.width / 2, self.height / 2)
+        time.sleep(1)
+        pg.press("enter")
+        time.sleep(1)
+        pg.hotkey("ctrl", "w")
+        pg.press("enter")
+        if number != self.contacts["Me"]:
+            self.send_message(self.contacts["Me"], f"Message to {number} at {datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} with content: {text}")
+
+    def send_to_contact(self, contactName:str, text:str):
+        self.send_message(self.contacts[contactName], text)
+
+    def count_notifs(self):
+        if os.path.exists("data\\whatsapp.txt"):
+            os.remove("data\\whatsapp.txt")
+
+        web.open("https://web.whatsapp.com/")
+        time.sleep(3)
+        pg.hotkey("ctrl", "s")
+        time.sleep(1)
+        saveFilename = os.getcwd() + "\\data\\whatsapp.txt"
+        pg.typewrite(saveFilename)
+        pg.press("enter")
+        time.sleep(5)
+        pg.hotkey("ctrl", "w")
+
+        for file in glob.glob("data\\whatsapp_files\\*.*"):
+            os.remove(file)
+        os.rmdir("data\\whatsapp_files")
+
+        with open("data\\whatsapp.txt", "r", encoding='cp437') as f:
+            soup = BeautifulSoup(f.read(), "html.parser")
+        
+        new_msgs = soup.find_all('div', {"class":"_1pJ9J"})
+        totalUnread = 0
+        for msg in new_msgs:
+            totalUnread += int(msg.find('span').text)
+        
+        return totalUnread
